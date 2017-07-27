1686aeeb1adc6373e7c62e24edcda078a3d352a0
Jim <jimorcheson@yahoo.ca>
2017-07-27 11:13:54 -0400

    New VS solution with CardLib and CardLibUnitTests projects. The projects contain CardSuit class and unit tests for the class.

---
+++ CardLib/CardSuit.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PlayingCards
+{
+    public class CardSuit
+    {
+        public enum SuitNames
+        {
+            Clubs,
+            Diamonds,
+            Hearts,
+            Spades
+        }
+
+        public CardSuit(SuitNames suit)
+        {
+            switch (suit)
+            {
+                case SuitNames.Clubs:
+                    suitString = "♣";
+                    break;
+                case SuitNames.Diamonds:
+                    suitString = "♦";
+                    break;
+                case SuitNames.Hearts:
+                    suitString = "♥";
+                    break;
+                case SuitNames.Spades:
+                    suitString = "♠";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("suit", suit, 
+                        "The suit value must be one of Clubs, Diamonds, Hearts, Spades.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return suitString;
+        }
+
+        private readonly string suitString;
+    }
+
+}
---
+++ CardLibUnitTests/CardSuitTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+using PlayingCards;
+
+namespace CardLibUnitTests
+{
+    public class CardSuitTests
+    {
+        [Theory]
+        [InlineData(CardSuit.SuitNames.Clubs, "♣")]
+        [InlineData(CardSuit.SuitNames.Diamonds, "♦")]
+        [InlineData(CardSuit.SuitNames.Hearts, "♥")]
+        [InlineData(CardSuit.SuitNames.Spades, "♠")]
+        public void CardSuitToStringTest(CardSuit.SuitNames suit, string suitString)
+        {
+            var cardSuit = new CardSuit(suit);
+
+            var card = String.Format("{0}", cardSuit);
+
+            Assert.Equal(suitString, card);
+        }
+
+        [Fact]
+        public void CardSuit3ConstructorInvalidValueTest()
+        {
+            Exception ex = Assert.Throws<ArgumentOutOfRangeException>(()
+                => new CardSuit((CardSuit.SuitNames)(-1)));
+
+            bool invalidSuit = ex.Message.Contains(
+                "The suit value must be one of Clubs, Diamonds, Hearts, Spades.");
+
+            Assert.True(invalidSuit);
+        }
+    }
+}
