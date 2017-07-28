15c29bc096e6051dbb5008b8b7395f197ae6682d
Jim <jimorcheson@yahoo.ca>
2017-07-28 06:56:15 -0400

    Added CardRank class and CardRankTests class with tests for CardRank.ToString.

---
+++ CardLib/CardRank.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlayingCards
+{
+    public class CardRank
+    {
+        public enum RankNames
+        {
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King,
+            Ace
+        }
+
+        static CardRank()
+        {
+            ranks.Add(RankNames.Two, "2");
+            ranks.Add(RankNames.Three, "3");
+            ranks.Add(RankNames.Four, "4");
+            ranks.Add(RankNames.Five, "5");
+            ranks.Add(RankNames.Six, "6");
+            ranks.Add(RankNames.Seven, "7");
+            ranks.Add(RankNames.Eight, "8");
+            ranks.Add(RankNames.Nine, "9");
+            ranks.Add(RankNames.Ten, "10");
+            ranks.Add(RankNames.Jack, "J");
+            ranks.Add(RankNames.Queen, "Q");
+            ranks.Add(RankNames.King, "K");
+            ranks.Add(RankNames.Ace, "A");
+        }
+
+        public CardRank(RankNames rank)
+        {
+            var hasKey = ranks.ContainsKey(rank);
+            if (hasKey)
+            {
+                this.rank = rank;
+            }
+            else
+            { 
+                throw new ArgumentOutOfRangeException("rank", rank,
+                    "The rank value is invalid.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return Rank;
+        }
+
+        public string Rank
+        {
+            get
+            {
+                return ranks[rank];
+            }
+        }
+
+        private RankNames rank;
+
+        static Dictionary<CardRank.RankNames, string> ranks =
+            new Dictionary<CardRank.RankNames, string>();
+    }
+}
---
+++ CardLibUnitTests/CardRankTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+using PlayingCards;
+
+namespace CardLibUnitTests
+{
+    public class CardRankTests
+    {
+        [Theory]
+        [InlineData(CardRank.RankNames.Two, "2")]
+        [InlineData(CardRank.RankNames.Three, "3")]
+        [InlineData(CardRank.RankNames.Four, "4")]
+        [InlineData(CardRank.RankNames.Five, "5")]
+        [InlineData(CardRank.RankNames.Six, "6")]
+        [InlineData(CardRank.RankNames.Seven, "7")]
+        [InlineData(CardRank.RankNames.Eight, "8")]
+        [InlineData(CardRank.RankNames.Nine, "9")]
+        [InlineData(CardRank.RankNames.Ten, "10")]
+        [InlineData(CardRank.RankNames.Jack, "J")]
+        [InlineData(CardRank.RankNames.Queen, "Q")]
+        [InlineData(CardRank.RankNames.King, "K")]
+        [InlineData(CardRank.RankNames.Ace, "A")]
+        public void TestToString(CardRank.RankNames testRank, string value)
+        {
+            var cardRank = new CardRank(testRank);
+
+            var rankString = string.Format("{0}", cardRank);
+
+            Assert.Equal(value, rankString);
+        }
+    }
+}
